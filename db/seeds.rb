c57bde2986235ee4718e6db4e252f516fefd20c0
Kevin Hughes <kevtufc@gmail.com>
2017-09-18 15:34:52 +0100

    Seeds and prettier notices

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+kev = User.create name: 'Kev', email: 'kev@example.com', password: 'password'
+team = Team.create title: 'Kev\'s Team'
+kev.teams << team
+plan = Plan.create title: 'Kev\'s Plan'
+
+1.upto(10) do |week|
+  plan.days << IndoorRide.create(title:  "Monday Indoor Ride", day_of_week: 1, week: week)
+  plan.days << CrossTrain.create(title:  "Cross Training",     day_of_week: 3, week: week)
+  plan.days << OutdoorRide.create(title: "Saturday Ride",      day_of_week: 6, week: week)
+  plan.days << OutdoorRide.create(title: "Club Ride",          day_of_week: 7, week: week)
+end
+
+team.starts_following(plan, on: Date.today)
